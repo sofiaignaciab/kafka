a05e280b0197f6b0e2ace43b5f599fed7e39fbee
sofiaignaciab <sofi.belmar9@gmail.com>
2024-05-28 00:44:50 -0400

    fix: add itemName

--- src/solicitudes/solicitud.service.ts
+++ src/solicitudes/solicitud.service.ts
@@ -7,12 +7,27 @@ export class SolicitudService {
   constructor(private prisma: PrismaService) {}
 
   async create(data: CreateSolicitudDto) {
-    return this.prisma.solicitud.create({
+    //get name with itemid
+    const compra = await this.prisma.compras.findUnique({
+      where: { id: Number(data.itemid) },
+    });
+
+    if (!compra) {
+      throw new Error('Item not found');
+    }
+
+    //create request
+    const solicitud = await this.prisma.solicitud.create({
       data: {
         correo: data.correo,
-        itemid: data.itemid,
+        itemid: Number(data.itemid),
         estado: 'recibido',
       },
     });
+    //return the request
+    return {
+      ...solicitud,
+      itemname: compra.itemname,
+    };
   }
 }
